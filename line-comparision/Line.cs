bef2798e556c5be6e643a0155da38eac7c6382de
arivusurya <arivusurya.3@gmail.com>
2023-04-24 08:51:32 +0530

    calculate the length of the line with calculate method

---
+++ line-comparision/Line.cs
@@ -0,0 +1,30 @@
+namespace Line_comparison
+{
+    public class Line
+    {
+        int x_1,y_1,x_2,y_2;
+        public Line(int x_1,int y_1,int x_2,int y_2){
+            this.x_1 =  x_1;
+            this.y_1=y_1;
+            this.x_2=x_2;
+            this.y_2=y_2;
+        }
+
+        public double Calculate(){
+            // A Length as 2 Points (x1, y1) and (x2, y2) - Length of a Line = sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2)
+            double Length = Math.Sqrt(Math.Pow((x_2-x_1),2)+Math.Pow((y_2-y_1),2));
+            return Length;
+        }
+
+        public static void Compare(Line line1,Line line2){
+            double Length1 = line1.Calculate();
+            double Length2 = line2.Calculate();
+
+            if(Length1 > Length2){
+                Console.WriteLine("The Line 1 Longer than Line2");
+            }else{
+                Console.WriteLine("The Line 2 longer than Line 1");
+            }
+        }
+    }
+}
---
+++ line-comparision/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Line_comparison{
+
+    public class Program {
+        public static void Main(string[] args)
+        {
+           Line line1 = new Line(2,3,5,7);
+           Line line2 = new Line(4,4,7,7);
+           Line.Compare(line1,line2);
+        }
+    }
+
+}
